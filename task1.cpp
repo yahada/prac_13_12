6e05066fefa361fff28967d08df3bbbdeef2b6b2
yahada <niadzy14@gmail.com>
2025-12-13 12:35:52 +0300

    task1's done

--- task1.cpp
+++ task1.cpp
@@ -15,6 +15,46 @@ struct Subj {
   const size_t * visited;
 };
 
+const Stud** copy(const Stud* const* src, const Stud** dest, size_t k) 
+{
+  for (size_t i = 0; i < k; ++i) {
+      dest[i] = src[i];
+  }
+  return dest + k;
+}
+bool contains(const Stud * const* stud, const Stud* s, size_t k)
+{
+  bool res = false;
+  for (size_t i = 0; i < k; ++i) {
+    res = res || stud[i] == s;
+  }
+  return res;
+}
+
+const Stud ** stud_union(size_t& r, const Subj& s1, const Subj& s2)
+{
+  const Stud ** uni = new const Stud*[s1.people + s2.people];
+  copy(s1.studs, uni, s1.people);
+  size_t count = s1.people;
+
+  for (size_t i = 0; i < s2.people; ++i) {
+    if (!contains(uni, s2.studs[i], count)) {
+      uni[count++] = s2.studs[i];
+    }
+  }
+  try {
+    const Stud** tmp = new const Stud*[count];
+    copy(uni, tmp, count);
+    delete [] uni;
+    uni = tmp;
+  } catch (...) {
+    delete [] uni;
+    throw;
+  }
+  r = count;
+  return uni;
+}
+
 int main()
 {
 
